a7698fd2f91ab9bf06454880d8fc936aeb62cf42
abhisheknaiidu <geekyabhi12@gmail.com>
2020-09-03 21:01:11 +0530

    Hoisting Basic

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('abhishek');
+name = 'abhishek'
+// var name
+let name // using let, we get reference error because of not following hoisting rules!
+console.log(name)
+// Hoisting is applicable just for var variables and not for let or const keywords
