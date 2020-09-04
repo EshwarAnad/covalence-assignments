caec46f300d16779ba7f5ed8e79fe8f9ffdf01e3
abhisheknaiidu <geekyabhi12@gmail.com>
2020-09-04 19:31:47 +0530

    some changes to closure

--- Context/index.js
+++ Context/index.js
@@ -8,6 +8,7 @@ for(var i=1; i<=100; i++) {
     var h3 = document.createElement('h3')
     h3.className = 'accusation' + i
     h3.innerHTML = 'Accusation' + ' ' + i
+    // because of var, we need to use closure here, so that we can take the snapshot of each and every Iteration with it's index.
     h3.addEventListener('click', function (index) {
         return function() {
             console.log('Accusation' + ' ' + index + ':' + 'I accuse' + ' ' + friendNames[index%5] + ' ' + 'with the' + ' ' + weapons[index%20] + ' ' + 'in the' + ' ' + locations[index%10]) 
